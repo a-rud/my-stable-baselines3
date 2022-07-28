325748f9e32d60fc3a0eec8c6d3ddc57a21bd331
Adrian Rudloff <80639001+a-rud@users.noreply.github.com>
2022-07-28 23:46:00 +0200

    Update docker build to ubuntu:20.04 and cuda11.3

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-GPU_PARENT=nvidia/cuda:10.1-cudnn7-runtime-ubuntu18.04
-CPU_PARENT=ubuntu:18.04
+#GPU_PARENT=nvidia/cuda:10.1-cudnn7-runtime-ubuntu18.04
+#CPU_PARENT=ubuntu:18.04
+
+CPU_PARENT=ubuntu:20.04
+GPU_PARENT=nvidia/cuda:11.3.1-base-ubuntu20.04
 
 TAG=arudl/my-stable-baselines3
 LATEST=latest
@@ -9,7 +12,7 @@ VERSION=$(cat ./stable_baselines3/version.txt) # version of SB3
 
 if [[ ${USE_GPU} == "True" ]]; then
   PARENT=${GPU_PARENT}
-  PYTORCH_DEPS="cudatoolkit=10.1"
+  PYTORCH_DEPS="cudatoolkit=11.3"
 else
   PARENT=${CPU_PARENT}
   PYTORCH_DEPS="cpuonly"
